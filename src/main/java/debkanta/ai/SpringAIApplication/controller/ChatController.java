e2ab4cc7f09ff0f230c6fdd43b8c9bef6ef8fa91
Debkanta Mitra <debkantamitra52@gmail.com>
2025-04-02 23:55:31 +0530

    ++ image

--- src/main/java/debkanta/ai/SpringAIApplication/controller/ChatController.java
+++ src/main/java/debkanta/ai/SpringAIApplication/controller/ChatController.java
@@ -2,23 +2,28 @@ package debkanta.ai.SpringAIApplication.controller;
 
 import debkanta.ai.SpringAIApplication.model.ChatAskRequestModel;
 import debkanta.ai.SpringAIApplication.service.ChatService;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import debkanta.ai.SpringAIApplication.service.ImageService;
+import org.springframework.ai.image.ImageResponse;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/chat")
 public class ChatController {
     private final ChatService chatService;
+    private final ImageService imageService;
 
-    public ChatController(ChatService chatService) {
+    public ChatController(ChatService chatService, ImageService imageService) {
         this.chatService = chatService;
+        this.imageService = imageService;
     }
 
     @PostMapping("ask")
-    public String getChatResponse(@RequestBody ChatAskRequestModel request) {
-        return chatService.getChatResponse(request.getPrompt());
+    public String getChatResponse(@RequestBody ChatAskRequestModel chatAskRequestModel) {
+        return chatService.getChatResponseWithOptions(chatAskRequestModel.getPrompt());
+    }
+
+    @GetMapping("generate-image")
+    public String getGeneratedImage(@RequestParam String prompt) {
+        return imageService.generateImage(prompt).getResult().getOutput().getUrl();
     }
 }
--- src/main/java/debkanta/ai/SpringAIApplication/service/ChatService.java
+++ src/main/java/debkanta/ai/SpringAIApplication/service/ChatService.java
@@ -1,6 +1,7 @@
 package debkanta.ai.SpringAIApplication.service;
 
 import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.openai.OpenAiChatOptions;
 import org.springframework.stereotype.Service;
@@ -14,14 +15,19 @@ public class ChatService {
     }
 
     public String getChatResponse(String prompt) {
+        return chatModel.call(prompt);
+    }
 
-        return chatModel.call(
+    public String getChatResponseWithOptions(String prompt) {
+        ChatResponse chatResponse = chatModel.call(
                 new Prompt(
                         prompt,
                         OpenAiChatOptions.builder()
                                 .model("gpt-4o-mini")
                                 .temperature(0.4)
                                 .build()
-                )).toString();
+                ));
+
+        return chatResponse.getResult().getOutput().getText();
     }
 }
