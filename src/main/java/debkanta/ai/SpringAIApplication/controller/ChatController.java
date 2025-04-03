496d55fc32696205e838e14f93b50979b9797d74
Debkanta Mitra <debkantamitra52@gmail.com>
2025-04-03 23:54:36 +0530

    ++ image with options

--- src/main/java/debkanta/ai/SpringAIApplication/controller/ChatController.java
+++ src/main/java/debkanta/ai/SpringAIApplication/controller/ChatController.java
@@ -3,8 +3,12 @@ package debkanta.ai.SpringAIApplication.controller;
 import debkanta.ai.SpringAIApplication.model.ChatAskRequestModel;
 import debkanta.ai.SpringAIApplication.service.ChatService;
 import debkanta.ai.SpringAIApplication.service.ImageService;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.ai.image.ImageResponse;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/chat")
@@ -23,7 +27,19 @@ public class ChatController {
     }
 
     @GetMapping("generate-image")
-    public String getGeneratedImage(@RequestParam String prompt) {
-        return imageService.generateImage(prompt).getResult().getOutput().getUrl();
+    public void getGeneratedImage(HttpServletResponse response, @RequestParam String prompt) throws IOException {
+       ImageResponse imageResponse = imageService.generateImage(prompt);
+       String imgUrl = imageResponse.getResult().getOutput().getUrl();
+
+       response.sendRedirect(imgUrl);
+    }
+
+    @GetMapping("generate-images")
+    public List<String> getGeneratedImages(@RequestParam String prompt) {
+        ImageResponse imageResponse = imageService.generateImagesWithOptions(prompt);
+
+        return imageResponse.getResults().stream()
+                .map(result -> result.getOutput().getUrl())
+                .toList();
     }
 }
--- src/main/java/debkanta/ai/SpringAIApplication/service/ImageService.java
+++ src/main/java/debkanta/ai/SpringAIApplication/service/ImageService.java
@@ -3,6 +3,7 @@ package debkanta.ai.SpringAIApplication.service;
 import org.springframework.ai.image.ImagePrompt;
 import org.springframework.ai.image.ImageResponse;
 import org.springframework.ai.openai.OpenAiImageModel;
+import org.springframework.ai.openai.OpenAiImageOptions;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,5 +17,15 @@ public class ImageService {
     public ImageResponse generateImage(String prompt) {
         return openAiImageModel.call(
                 new ImagePrompt(prompt));
+    }
+
+    public ImageResponse generateImagesWithOptions(String prompt) {
+        return openAiImageModel.call(
+                new ImagePrompt(prompt, OpenAiImageOptions.builder()
+                        .withModel("dall-e-2")
+                        .withN(4)
+                        .style("natural")
+                        .build())
+        );
     }
 }
